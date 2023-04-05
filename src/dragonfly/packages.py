b9f79734787bdc78ac89cba0fbdd5abffc020dbc
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-04-05 16:59:28 -0500

    setup mvp

---
+++ src/dragonfly/packages.py
@@ -0,0 +1,56 @@
+"""Interactions with parsing package metadata and contents"""
+
+import tarfile
+from io import BytesIO
+
+import aiohttp
+
+
+async def find_package_source_download_url(
+    http_session: aiohttp.ClientSession, package_title: str
+) -> str | None:
+    """Find the `.tar.gz` download for a package.
+    Return `None` if there are no files or if only wheels are available."""
+    metadata_url = f"https://pypi.org/pypi/{package_title}/json"
+    async with http_session.get(metadata_url) as response:
+        package_metadata = await response.json()
+
+    if "urls" not in package_metadata:
+        return None
+
+    for url in package_metadata["urls"]:
+        if url["url"].endswith(".tar.gz"):
+            return url["url"]
+
+    return None
+
+
+async def fetch_package_contents(
+    http_session: aiohttp.ClientSession, package_source_download_url: str
+) -> dict[str, str]:
+    """Return a dictionary mapping filenames to content"""
+    files = {}
+    buffer = BytesIO()
+    async with http_session.get(package_source_download_url) as response:
+        buffer.write(await response.content.read())
+    buffer.seek(0)
+    with tarfile.open(fileobj=buffer) as file:
+        for tarinfo in file:
+            if tarinfo.isreg():
+                files[tarinfo.name] = (
+                    file.extractfile(tarinfo.name)
+                    .read()
+                    .decode(encoding="UTF-8", errors="ignore")
+                )
+    return files
+
+
+def search_contents(rules, files: dict[str, str]) -> list[tuple[str, "yara matches"]]:
+    """Check a directory for malicious files and return the matches"""
+    results = []
+    for file_name, file_contents in files.items():
+        matches = rules.match(data=file_contents)
+        if matches:
+            results.append((file_name, matches))
+
+    return results
---
+++ src/dragonfly/rules.py
@@ -0,0 +1,33 @@
+"""Interactions with Yara rules"""
+
+from io import BytesIO
+from os import getenv
+from typing import Final
+from zipfile import ZipFile
+
+import aiohttp
+import yara
+
+RULES_URL: Final[str] = "https://github.com/SkeletalDemise/DragonFly/archive/refs/heads/main.zip"
+HEADERS: Final[dict[str, str]] = {"Authorization": f"Bearer {getenv('DRAGONFLY_GITHUB_TOKEN')}"}
+
+
+async def _fetch_rules() -> dict[str, str]:
+    """Return a dictionary mapping filenames to content"""
+    files = {}
+    buffer = BytesIO()
+    async with aiohttp.ClientSession(raise_for_status=True, headers=HEADERS) as http_session:
+        async with http_session.get(RULES_URL) as response:
+            buffer.write(await response.content.read())
+    buffer.seek(0)
+    with ZipFile(buffer) as zip_file:
+        for file_name in zip_file.namelist():
+            if file_name.endswith(".yara"):
+                files[file_name.removesuffix(".yara")] = zip_file.read(file_name).decode()
+    return files
+
+
+async def get_rules() -> "compiled yara rules":
+    """Fetch and compile the rules"""
+    sources = await _fetch_rules()
+    return yara.compile(sources=sources)
