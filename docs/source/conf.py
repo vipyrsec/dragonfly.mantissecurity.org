59d5790be83ddefdfef263f942b568cc3f789727
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-04-12 22:41:57 -0500

    rename project
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 from importlib.metadata import metadata
 
-project_metadata = metadata("dragonfly.letsbuilda.dev")
+project_metadata = metadata("dragonfly.mantissecurity.org")
 project: str = project_metadata["Name"]
 release: str = project_metadata["Version"]
 REPO_LINK: str = project_metadata["Project-URL"].replace("repository, ", "")
